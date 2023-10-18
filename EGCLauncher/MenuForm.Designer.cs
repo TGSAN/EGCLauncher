b65a5ad060a51d842678473047205a49519935f4
TGSAN <h@iloli.tv>
2023-10-19 01:27:50 +0800

    Fix HiDPI

--- EGCLauncher/MenuForm.Designer.cs
+++ EGCLauncher/MenuForm.Designer.cs
@@ -98,8 +98,8 @@
             // 
             // MenuForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(284, 361);
             this.Controls.Add(this.launcherButton);
